d59c59b2b6f84fdb26f503897712976e3abbb7ad
Dustin <bhappydustin@gmail.com>
2022-02-09 16:57:36 +0800

    add lagou-edu-mobile

---
+++ lagou-edu-mobile/js/error-page.877f2e50.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["error-page"],{"4cf1":function(e,r,a){"use strict";a.r(r);var n=function(){var e=this,r=e.$createElement,a=e._self._c||r;return a("div",{staticClass:"error-page"},[e._v("页面不见了")])},t=[],s={name:"ErrorPage"},c=s,o=a("2877"),i=Object(o["a"])(c,n,t,!1,null,"bede297e",null);r["default"]=i.exports}}]);
